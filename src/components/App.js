8b299715cb437813b4617550ffe81da610cb8153
gauri38 <kumbhar.gauri@gmail.com>
2020-12-03 00:41:28 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <>
+      <button data-testid="button-restart">Restart</button>
+      <button data-testid="button-prev">Prev</button>
+      <button data-testid="button-next">Next</button>
+    </>
   )
 }
 
