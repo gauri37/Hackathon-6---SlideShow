4ded037d625fc4b5ca0e714d48e3a68ed576f014
gauri38 <kumbhar.gauri@gmail.com>
2020-12-09 18:29:15 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import slides from '../data';
 const App = () => {
+  const [index, setIndex] = useState(0);
+  
   return (
-    <>
-      <button data-testid="button-restart">Restart</button>
-      <button data-testid="button-prev">Prev</button>
-      <button data-testid="button-next">Next</button>
-    </>
+    <div>
+      <h1 data-testid="title">{slides[index].title}</h1>
+      <p data-testid="text">{slides[index].text}</p>
+      <button data-testid="button-restart" disabled={index===0} onClick={()=> setIndex(0)}>Restart</button>
+      <button data-testid="button-prev" disabled={index===0} onClick={()=>{
+        if(index>0)
+          setIndex(index-1)
+      }}>Prev</button>
+      <button data-testid="button-next" disabled={index===slides.length-1} onClick={()=> {
+        if(index<slides.length-1)
+            setIndex(index+1)
+        }}>Next</button>
+    </div>
   )
 }
-
-
-export default App;
